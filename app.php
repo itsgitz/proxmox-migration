6d9abb8a03afdf6090f4e7d0c2eb00520e4555d2
Anggit M Ginanjar <anggit@isi.co.id>
2020-04-10 23:08:54 +0700

    Adding arguments parsing condition using system class

--- app.php
+++ app.php
@@ -2,17 +2,27 @@
 
 /**
  * Proxmox Migration Script
- * author: Developer Team @ Infinys System Indonesia
+ * 
+ * @author: Developer Team @ Infinys System Indonesia
+ * @link: https://isi.co.id
+ * 
  */
 
-include './configuration/loader.php';
+require __DIR__ . '/configuration/loader.php';
 
 
 use ProxmoxMigration\DB\CSV;
 use ProxmoxMigration\DB\Database;
 use ProxmoxMigration\DB\DatabaseRepository as DB_REPO;
+use ProxmoxMigration\DB\System;
 
 
+
+// define system class
+$sys = new System();
+
+// Check and define arguments
+$arguments = $sys->getArguments($argv);
 
 // Define and get a database connection ...
 $db = new Database();
@@ -20,11 +30,14 @@ $db = new Database();
 // Define csv class
 $csv = new CSV();
 
+// check if mode is development mode
+$isDev = $sys->isDev($arguments['mode']);
+
 // Show list of tables that will be migrate
-echo DB_REPO::showListTables();
+DB_REPO::showListTables($isDev);
 
 // Get arguments
-switch ($argv[1]) {
+switch ($arguments['action']) {
 
     /**
      * Query and Export process
@@ -33,11 +46,29 @@ switch ($argv[1]) {
         // Check database connection
         if (!$db->isConnectionError()) {
 
-            // query get users data
-            $users = $db->getQuery(DB_REPO::USERS_TABLENAME);
+            /**
+             * Only test:
+             */
+            // // query get users data
+            // $users = $db->getQuery(DB_REPO::USERS_TABLENAME);
+            // // export data to csv
+            // $csv->exportToCsv(DB_REPO::USERS_TABLENAME, $users);
 
-            // export data to csv
-            $csv->exportToCsv(DB_REPO::USERS_TABLENAME, $users);
+
+            /**
+             * DEV MODE
+             */
+            // export tblhosting data
+            // $tblhosting = $db->getQuery(DB_REPO::);
+            // export tblcustomfields
+            // export proxmoxVPS_Users
+            // export proxmoxVPS_IP
+            // export mg_proxmox_addon_ip
+            // export mod_proxmox_change_password_log
+            
+            /**
+             * PROD MODE
+             */
 
         } else {
 
@@ -51,11 +82,10 @@ switch ($argv[1]) {
      * Import process
      */
     case DB_REPO::IMPORT:
-        var_dump($db->runImportData(DB_REPO::USERS_CSV_FILES, DB_REPO::USERS_TABLENAME, DB_REPO::USERS_COLUMNS));
+        // var_dump($db->runImportData(DB_REPO::USERS_CSV_FILES, DB_REPO::USERS_TABLENAME, DB_REPO::USERS_COLUMNS));
 
         break;
 
     default:
-        echo DB_REPO::DB_INVALID_ARGUMENT;
         break;
 }
