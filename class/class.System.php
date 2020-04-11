7645aba3173c75a6fbeaccda485e7ea62e591859
Anggit M Ginanjar <anggit@isi.co.id>
2020-04-11 12:09:53 +0700

    Add isDev method and some constant variables

--- class/class.System.php
+++ class/class.System.php
@@ -7,6 +7,8 @@ use ProxmoxMigration\DB\DatabaseRepository as DB_REPO;
 
 class System
 {
+    const ACTION = 'action';
+    const MODE = 'mode';
     const DEVELOPMENT_MODE = 'dev';
     const PRODUCTION_MODE = 'prod';
 
@@ -40,27 +42,35 @@ class System
         $action = $argv[1];
         $mode = $argv[2];
         
-        if (strcmp($action, DB_REPO::EXPORT) && strcmp($action, DB_REPO::IMPORT)) {
+        if (strcmp($action, DB_REPO::EXPORT) != 0 && strcmp($action, DB_REPO::IMPORT) != 0) {
             echo "[ERROR]: Invalid arguments (action)! \n\n";
             echo "It should be export or import\n\n";
             die();
         } 
-        
-        if (strcmp($mode, self::DEVELOPMENT_MODE) && strcmp($mode, self::PRODUCTION_MODE)) {
+
+        if (strcmp($mode, self::DEVELOPMENT_MODE) != 0 && strcmp($mode, self::PRODUCTION_MODE) != 0) {
             echo "[ERROR]: Invalid arguments (mode)! \n\n";
             echo "It should be dev (for development environment) or prod (for production environment) \n\n";
             die();
         }
 
         return [
-            'action' => $action,
-            'mode' => $mode,
+            self::ACTION => $action,
+            self::MODE => $mode,
         ];
     }
 
+    /**
+     * isDev
+     * 
+     * check if application run in dev mode
+     * 
+     * @param string mode
+     * @return bool true if "dev", else return false
+     */
     public function isDev($mode)
     {
-        if (strcmp($mode, self::DEVELOPMENT_MODE)) {
+        if (strcmp($mode, self::DEVELOPMENT_MODE) == 0) {
             return true;
         } else {
             return false;
