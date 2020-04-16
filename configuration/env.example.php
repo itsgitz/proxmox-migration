817211d60de879a12d397524eb69493b6cc870e1
Anggit M Ginanjar <anggit@isi.co.id>
2020-04-17 01:07:57 +0700

    Add env variable for whmcs api

--- configuration/env.example.php
+++ configuration/env.example.php
@@ -11,6 +11,9 @@ $ENV = [
     'DB_SERVERNAME' => '',
     'DB_USERNAME' => '',
     'DB_PASSWORD' => '',
+    'WHMCS_API_IDENTIFIER' => '',
+    'WHMCS_API_SECRET' => '',
+    'WHMCS_API_URL' => '',
 ];
 
 foreach ($ENV as $k => $v) {
